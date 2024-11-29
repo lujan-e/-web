e00a79247fd19264dd4f7c27504a17be44b415c6
lujan <espinozamarialujan94@gamil.com>
2024-11-29 13:55:45 -0300

    segundo commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user_detail/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+    path('user_update/<int:pk>/', views.UserUpdate.as_view(), name='user_update'),
     path('password_change/', views.PasswordChange.as_view(), name='password_change'),
     path('password_change/done/', views.PasswordChangeDone.as_view(), name='password_change_done'),
      path('user_delete/<int:pk>/', views.UserDelete.as_view(), name='user_delete'),
